410cc60f4456bcc5b6904da548e918195315b8f5
artuone83 <wozniaka83@gmail.com>
2022-08-07 18:53:38 +0200

    new date from iso string

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ appDiv.innerHTML = `<h1>new Date sandbox</h1>`;
 const subtractDays = (date, subtructDays) => {
   return date.setDate(date.getDate() - subtructDays);
 };
-const date = new Date(2022, 7, 6);
+const date = new Date(1659855635 * 1000);
 
 console.log(date.toISOString());
 console.log(date.toLocaleString('pl-PL'));
 
-const msToSec = Math.floor(new Date(2022, 7, 6).getTime() / 1000);
+const msToSec = Math.floor(new Date('2022-08-07').getTime() / 1000);
 console.log(msToSec);
