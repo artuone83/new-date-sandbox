be14859b4dec5acc42c0d3448013c04c21ad952c
artuone83 <wozniaka83@gmail.com>
2022-08-08 13:04:20 +0200

    set hours to midnight

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>new Date sandbox</h1>`;
 
-const subtractDays = (date, subtructDays) => {
+const ONE_SECOND_IN_MILLISECONDS = 1000;
+// date: Date
+const getTimestamp = (date) => {
+  const currentDate = new Date(date.setHours(0, 0, 0, 0));
+
+  return Math.floor(currentDate.getTime() / ONE_SECOND_IN_MILLISECONDS);
+};
+// {dateFrom: Date, addDays: number} return milliseconds
+const addDays = ({ dateFrom, addDays }) => {
+  return dateFrom.setDate(dateFrom.getDate() + addDays);
+};
+
+const subtractDays = ({ date, subtructDays }) => {
   return date.setDate(date.getDate() - subtructDays);
 };
-const date = new Date(1659855635 * 1000);
 
-console.log(date.toISOString());
-console.log(date.toLocaleString('pl-PL'));
+const date = new Date(1659909600 * 1000);
 
-const msToSec = Math.floor(new Date('2022-08-07').getTime() / 1000);
-console.log(msToSec);
+console.log('new Date(date.toString()) --> ', new Date(date.toString()));
+console.log('date.toISOString() -->', date.toISOString());
+console.log('date.toLocaleString', date.toLocaleString('pl-PL'));
+
+console.log('getTimestamp', getTimestamp(date));
+
+console.log('addDays', addDays({ dateFrom: date, addDays: 1 }));
