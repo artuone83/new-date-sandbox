3f403d98bd352f38859d9d7979966e30e6648d0a
artuone83 <32961497+artuone83@users.noreply.github.com>
2022-09-09 06:43:02 +0200

    start end of day

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>new Date sandbox</h1>`;
+appDiv.innerHTML = `<h1>new Date sandbox</h1><p>Follow Console logs 👇</p>`;
 
 const ONE_SECOND_IN_MILLISECONDS = 1000;
 // date: Date
@@ -12,16 +12,31 @@ const getTimestamp = (date) => {
 
   return Math.floor(currentDate.getTime() / ONE_SECOND_IN_MILLISECONDS);
 };
-// {dateFrom: Date, addDays: number} return milliseconds
-const addDays = ({ dateFrom, addDays }) => {
-  return dateFrom.setDate(dateFrom.getDate() + addDays);
+// {date: Date, addDays: number} return milliseconds
+const addDays = ({ date, addDays }) => {
+  return new Date(date).setDate(date.getDate() + addDays);
 };
 
 const subtractDays = ({ date, subtructDays }) => {
-  return date.setDate(date.getDate() - subtructDays);
+  return new Date(date).setDate(date.getDate() - subtructDays);
 };
 
-const date = new Date(1659909600 * 1000);
+// date: Date | string | number, return milliseconds
+export const startOfDay = (date) => {
+  return new Date(date).setHours(0, 0, 0, 0);
+};
+
+// date: Date | string | number, return milliseconds
+export const endOfDay = (date) => {
+  return new Date(date).setHours(23, 59, 59, 999);
+};
+
+// milliseconds: number
+export const getTimestampInSeconds = (milliseconds) => {
+  return Math.floor(milliseconds / ONE_SECOND_IN_MILLISECONDS);
+};
+
+const date = new Date(1662760800000);
 
 console.log('new Date(date.toString()) --> ', new Date(date.toString()));
 console.log('date.toISOString() -->', date.toISOString());
@@ -29,4 +44,4 @@ console.log('date.toLocaleString', date.toLocaleString('pl-PL'));
 
 console.log('getTimestamp', getTimestamp(date));
 
-console.log('addDays', addDays({ dateFrom: date, addDays: 1 }));
+console.log('addDays', addDays({ date, addDays: 1 }));
